76286d79559300f186a0b9087a0879343a4296cd
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-06-13 14:09:52 +0300

    update to 25.1.2+

--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 
 import './assets/main.css';
 
 const app = createApp(App);
-const shadowHost = document.getElementById('app') as HTMLElement;
-const shadowRoot = shadowHost.attachShadow({ mode: 'open' }) as any;
 
-app.mount(shadowRoot);
+app.use(router);
+
+app.mount('#app');
